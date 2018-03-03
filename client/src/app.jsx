d080885cb9b8b8c121a216d04f516739d9aeb8b3
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-02 16:35:13 -0800

    client now sends a GET request

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import { InfoList } from './infoList.jsx';
 import { Map } from './map.jsx';
 
-var App = (props) => { //props: restaurant object
+var server = location.origin || 'http://localhost:3000';
 
-  return (
-    <div className="app">
-      <InfoList restaurant={props.restaurant} />
-      <Map geometry={props.restaurant.geometry} />
-    </div>
-  );
-};
+class App extends React.Component { //props: restaurant object
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurant: props.restaurant
+    }
+  }
+
+  componentDidMount() {
+    this.getRestaurant('ChIJP5PrLYSAhYARBcWhJXs55P4');
+  }
+
+  getRestaurant (id) {
+    axios.get(server + '/restaurants', {
+      params: {
+        id: id
+      }
+    }).then((result) => {
+      console.log('received:', result);
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <InfoList restaurant={this.state.restaurant} />
+        <Map geometry={this.state.restaurant.geometry} />
+      </div>
+    );
+  }
+}
 
 export { App };
