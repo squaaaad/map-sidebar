00dacc2ad10b00963bdd14f27a7d09044ccec0d8
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 11:18:48 -0800

    changed database name

--- db/models/restaurant.js
+++ db/models/restaurant.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-var db = mongoose.connect('mongodb://localhost/wegot');
+var db = mongoose.connect('mongodb://localhost/wegot-sidebar');
 
 var restaurantSchema = mongoose.Schema({
   result: {
