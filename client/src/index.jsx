9da095a9356ce299b2a5ae23102a1c39c9298a6c
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 13:06:19 -0800

    proxy server changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,6 @@ import React from 'react';
 import { App } from './app.jsx';
 
 var restaurantId = location.pathname.split('restaurants/')[1];
+restaurantId = restaurantId.substring(0, restaurantId.length - 1);
 
 ReactDOM.render(<App restaurantId={restaurantId} restaurant={null}/>, document.getElementById('sidebar-app'));
