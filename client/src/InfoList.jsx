22ac6910601d02d90016ccb7f5059bedcb260176
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-06 20:51:24 -0800

    fixed a bug where holes in data would throw errors

--- client/src/InfoList.jsx
+++ client/src/InfoList.jsx
@@ -42,18 +42,24 @@ var InfoList = (props) => {
   );
 };
 
-var InfoListElement = (props) => (
-  <div className="sidebar-flexbox-row sidebar-info-list-element">
-    <div className="sidebar-info-list-element-icon">
-      <i className={props.info.icon} />
-    </div>
-    <div className="sidebar-info-list-text">
-      <a className="sidebar-anchor" href={props.info.link.url}
-        target={props.info.link.newTab ? '_blank' : ''}>
-        {props.info.text}
-      </a>
-    </div>
-  </div>
-);
+var InfoListElement = (props) => {
+  if (!props.info.text) {
+    return <div></div>;
+  } else {
+    return (
+      <div className="sidebar-flexbox-row sidebar-info-list-element">
+        <div className="sidebar-info-list-element-icon">
+          <i className={props.info.icon} />
+        </div>
+        <div className="sidebar-info-list-text">
+          <a className="sidebar-anchor" href={props.info.link.url}
+            target={props.info.link.newTab ? '_blank' : ''}>
+            {props.info.text}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export {InfoList, InfoListElement, OpeningHours};
--- client/src/OpeningHours.jsx
+++ client/src/OpeningHours.jsx
@@ -69,6 +69,11 @@ class OpeningHours extends React.Component {
   }
 
   render() {
+    try {
+      var testPropData = this.props.info.data.periods[0].close;
+    } catch(error) {
+      return <div></div>;
+    }
     var openNow = this.getOpenNow();
     var timeRange = this.getTimeRange();
 
@@ -99,50 +104,49 @@ class OpeningHours extends React.Component {
   }
 }
 
-var Periods = (props) => {
-  var periodDivs = _.map(props.weekdayText, (period, index) => {
-    var weekday = period.split(': ')[0];
-    var time1;
-    var time2;
-    if (period.includes(', ')) {
-      time1 = period.split(': ')[1].split(', ')[0];
-      time2 = period.split(': ')[1].split(', ')[1];
-    } else {
-      time1 = period.split(': ')[1];
-      time2 = null;
-    }
+  var Periods = (props) => {
+    var periodDivs = _.map(props.weekdayText, (period, index) => {
+      var weekday = period.split(': ')[0];
+      var time1;
+      var time2;
+      if (period.includes(', ')) {
+        time1 = period.split(': ')[1].split(', ')[0];
+        time2 = period.split(': ')[1].split(', ')[1];
+      } else {
+        time1 = period.split(': ')[1];
+        time2 = null;
+      }
 
-    var periodObj = {
-      weekday: weekday,
-      time1: time1,
-      time2: time2
-    }
+      var periodObj = {
+        weekday: weekday,
+        time1: time1,
+        time2: time2
+      }
 
-    var weekdayNum = new Date(Date.now()).getDay() - 1;
-    if (weekdayNum === -1) {
-      weekdayNum = 6;
-    }
+      var weekdayNum = new Date(Date.now()).getDay() - 1;
+      if (weekdayNum === -1) {
+        weekdayNum = 6;
+      }
 
-    return (
-      <div key={index} className="sidebar-flexbox-col sidebar-periods-element"
-        style={{'fontWeight': weekdayNum === index ? 'bold' : 'normal'}} >
-        <div className="sidebar-flexbox-row sidebar-periods-element-info">
-          <div className="sidebar-periods-element-day" >{periodObj.weekday}</div>
-          <div className="sidebar-periods-element-time">{periodObj.time1}</div>
+      return (
+        <div key={index} className="sidebar-flexbox-col sidebar-periods-element"
+          style={{'fontWeight': weekdayNum === index ? 'bold' : 'normal'}} >
+          <div className="sidebar-flexbox-row sidebar-periods-element-info">
+            <div className="sidebar-periods-element-day" >{periodObj.weekday}</div>
+            <div className="sidebar-periods-element-time">{periodObj.time1}</div>
+          </div>
+          <div className="sidebar-periods-element-info-additional">
+            <div className="sidebar-periods-element-time">{periodObj.time2}</div>
+          </div>
         </div>
-        <div className="sidebar-periods-element-info-additional">
-          <div className="sidebar-periods-element-time">{periodObj.time2}</div>
-        </div>
-      </div>
-    )
-
-  });
+      )
+    });
 
   return (
       <div className="sidebar-flexbox-col sidebar-periods">
         {periodDivs}
       </div>
   )
-}
+};
 
 export { OpeningHours, Periods};
