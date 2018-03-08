08aab2dc35dc33d00a21e4bcc9649b8ca75c4ac3
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 17:15:07 -0800

    bugfix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,9 @@ import '../dist/styles.css';
 import '../dist/fontawesome-all.min.js';
 
 var restaurantId = location.pathname.split('restaurants/')[1];
-restaurantId = restaurantId.substring(0, restaurantId.length - 1);
+console.log('restaurant id is', restaurantId)
+if (restaurantId[restaurantId.length - 1] === '/') {
+  restaurantId = restaurantId.substring(0, restaurantId.length - 1);
+}
 
 ReactDOM.render(<App restaurantId={restaurantId} restaurant={null}/>, document.getElementById('sidebar-app'));
