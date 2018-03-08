6a89adbff5edbf12400b07e7d397a860682ea78a
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 16:49:56 -0800

    bundles icons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { App } from './app.jsx';
 import '../dist/styles.css';
+import '../dist/fontawesome-all.min.js';
 
 var restaurantId = location.pathname.split('restaurants/')[1];
 restaurantId = restaurantId.substring(0, restaurantId.length - 1);
