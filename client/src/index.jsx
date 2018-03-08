90d81022d1e66d3f39a63d14fe152f5ca461dd42
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 16:39:41 -0800

    now bundles css, reverted back to restaurants/:id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { App } from './app.jsx';
+import '../dist/styles.css';
 
 var restaurantId = location.pathname.split('restaurants/')[1];
 restaurantId = restaurantId.substring(0, restaurantId.length - 1);
--- server/routers/restaurants.js
+++ server/routers/restaurants.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var getRestaurantById = require('../../db/controllers/getRestaurantById.js');
 
-router.use('/:id/sidebar', express.static('client/dist'));
+router.use('/:id', express.static('client/dist'));
 
 module.exports = router;
