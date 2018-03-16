ccd68d1aba76a8f8e5a736f248574a16e85d5e90
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-15 21:05:16 -0700

    creat new version of json to csv conversion script for sql w/o jsonb

--- seedDB/fakerJSONtoCSVnoPeriods.py
+++ seedDB/fakerJSONtoCSVnoPeriods.py
@@ -31,37 +31,38 @@ with io.open(File_in, mode="r", encoding="utf-8") as jsonFile:
 #open write stream
     with open(File_out_places, mode='wt', encoding="utf-8") as places:
         placesCSV = csv.writer(places)
-        placesCSV.writerow(["place_id", "name", "formatted_address", "international_phone_number", "website", "url", "open_now", "open_periods", "weekday_text", "lat", "lon"])
+        placesCSV.writerow(["place_id", "name", "formatted_address", "international_phone_number", "website", "url", "open_now", "lat", "lon"])
         with open(File_out_openHours, mode='wt', encoding="utf-8") as openHours:
             openHoursCSV = csv.writer(openHours)
-            openHoursCSV.writerow(["place_id", "openday", "opentime", "closeday", "closetime"])
-            with open(File_out_json, 'wt') as openPeriodsJSON:
-                i = 0
-                for documentRow in jsonFile:
-                    i += 1
-                    if i % 100000 == 0:
-                        print('proceced ' + str(i) + ' rows')
-                    #print(documentRow)
-                    document = json.loads(documentRow)['result'];
-                    
-                    placesRow = [document['place_id'], document['name'], document['formatted_address'], document['international_phone_number'], document['website'], document['url'], document['opening_hours']['open_now'], document['opening_hours']['weekday_text'], document['geometry']['location']['lat'], document['geometry']['location']['lng']]
+            openHoursCSV.writerow(["place_id", "openday", "opentime", "closeday", "closetime", "weekday_text"])
+            # with open(File_out_json, 'wt') as openPeriodsJSON:
+            i = 0
+            for documentRow in jsonFile:
+                i += 1
+                if i % 100000 == 0:
+                    print('proceced ' + str(i) + ' rows')
+                #print(documentRow)
+                document = json.loads(documentRow)['result'];
                 
-                    placesCSV.writerow(placesRow)
-                    
-        
-                    
-                    # for period in document['opening_hours']['periods']:
-                    #     #add to places csv
-                    #     periodRow = [document['place_id'], period['open']['day'], period['open']['time'], period['close']['day'], period['close']['time']]
-                    #     openHoursCSV.writerow(periodRow)
-                    #     
-                    #     #add to json
-                    #     
-                    #     jsonRow = '{ place_id: ' + document['place_id'] + ', periods: ' + json.dumps(period) + '}'
-                    #     
-                    #     openPeriodsJSON.write(jsonRow)
-                    #     openPeriodsJSON.write("\n")
+                placesRow = [document['place_id'], document['name'], document['formatted_address'], document['international_phone_number'], document['website'], document['url'], document['opening_hours']['open_now'], document['geometry']['location']['lat'], document['geometry']['location']['lng']]
             
+                placesCSV.writerow(placesRow)
+                
+    
+                j = 0
+                for period in document['opening_hours']['periods']:
+                    #add to places csv
+                    periodRow = [document['place_id'], period['open']['day'], period['open']['time'], period['close']['day'], period['close']['time'], document['opening_hours']['weekday_text'][j]]
+                    j += 1;
+                    openHoursCSV.writerow(periodRow)
+                    
+                    # #add to json
+                    # 
+                    # jsonRow = '{ place_id: ' + document['place_id'] + ', periods: ' + json.dumps(period) + '}'
+                    # 
+                    # openPeriodsJSON.write(jsonRow)
+                    # openPeriodsJSON.write("\n")
+        
             
         
             
