a53e3517c4f4690ef09f9a3983d3cddb2f744ba9
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-24 13:20:18 -0700

    add debug statements to metrics

--- metrics/restaurants_api_stats.js
+++ metrics/restaurants_api_stats.js
@@ -3,7 +3,9 @@ statsd_server = process.env.STATSD_SERVER || '127.0.0.1';
 statsd_port = process.env.STATSD_PORT || 8125;
 
 const instruments = function() {
+  console.log('process.env.NOSTATS', process.env.NOSTATS);
   if (!process.env.NOSTATS) {
+    console.log('instrument running')
     statsd_client = new instrument.StatsD(statsd_server, statsd_port);
     statsd_instrument = new instrument.StatsDInstrumentation(statsd_client);
 
