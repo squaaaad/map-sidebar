915c63abb87807c92b1cd683085cd70f8deac05d
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-26 19:48:59 -0700

    remove console log when cached data retrieved

--- cache/redis.js
+++ cache/redis.js
@@ -6,7 +6,7 @@ const CACHE_TIME = process.env.CACHE_TIME || 6;
 
 const client = redis.createClient(REDIS_PORT);
 client.on("error", function (err) {
-    console.log("Error " + err);
+    console.log("Redis Error " + err);
 });
 
 //client.setex('key', CACHE_TIME, 'value');
@@ -23,7 +23,7 @@ const retrieve = (req, res, next) => {
       console.log (err);
     }
     if (data != null) {
-      console.log('retrieved cached data');
+      //console.log('retrieved cached data');
       res.send(JSON.parse(data));
     } else {
       next();
